bcc54644bb96819d6783181ec0f3b536acc32ea0
Syed Osama Hussain <osamahussain38@gmail.com>
2020-07-27 23:38:20 +0500

    Search integrated with back end.

--- routes/content.js
+++ routes/content.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Content } = require('../models/content');
 const childProc = require('child_process');
+const searcher = require('../searcher');
 
 
 router.get("/",async (req,res) => {
@@ -20,6 +21,26 @@ router.post("/",  (req,res) => {
         res.send("Done")
     })
     
-})
+});
+
+
+router.get("/:id", async (req,res) => {
+    const content = await Content.findById(req.params.id).select("-__v");
+
+    if (!content)
+    return res
+      .status(404)
+      .send("The result with given Id was not found.");
+
+    res.send(content);
+    
+});
+
+
+router.post("/search", async (req,res) => {
+    const content = await Content.find({});
+    const result = searcher(req.body.query,content);
+    res.send(result);
+});
 
 module.exports = router;
--- searcher.js
+++ searcher.js
@@ -1,35 +1,21 @@
-const query = process.argv[2].replace(/-/g, ' ').toLowerCase();
-const data = require('./indexedData.json');
 
 
-function Search(){
-    console.log(query)
-    let result = {};
+module.exports = function Search(query,data){
+    let result = [];
+
     for(let i=0;i<data.length;i++){
-        // console.log(data[i])
         const item = data[i];
         if(item.tags.some(tag => query.includes(tag))){
-            result[item.id] = item;
-            console.log(`found in tags: ${item.tags}`)
+            result.push(item);
+            // console.log(`found in tags: ${item.tags}`)
         }else if(item.content.indexOf(query) !== -1){
-            result[item.id] = item;
-            console.log(`found in content: ${item.content}`)
+            result.push(item);
+            // console.log(`found in content: ${item.content}`)
         }
     }
 
-    console.log('#########');
-    
-    if(Object.keys(result).length === 0) console.log("Sorry! No result found.")
-    
-    for (let id in result) {
-      const item = result[id];
-      console.log(`${item.title} - ${item.heading}`);
-      console.log(`Link: ${item.heading_url}`);
-      console.log();
-      console.log(item.content);
-      console.log('@@@@@@@@@');
-    }
+    if(result.length === 0) return Error("No result found.");
+
+    return result;
 
 }
-
-Search();
